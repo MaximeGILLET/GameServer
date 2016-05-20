3ced94a63832ce50fbfc54d2c292a6b6de449559
maxime gillet <gilet666@hotmail.fr>
2016-05-20 21:42:57 +0200

    first

---
+++ Assets/GlobalSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GlobalSystem : NetworkBehaviour {
+
+    bool isAtStartup = true;
+    public GameObject ChatLineModel;
+    public Transform ChatContainer;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && isAtStartup)
+        {
+            Debug.Log("Server Started..");
+            NetworkServer.Listen(8888);
+            NetworkServer.RegisterHandler(MsgType.Ready, OnPlayerReadyMessage);
+
+            isAtStartup = false;
+        }
+    }
+
+    [ClientCallback]
+    void FixedUpdate()
+    {
+        CmdDisplayClientMessage();
+    }
+
+    public void OnPlayerReadyMessage(NetworkMessage netMsg)
+    {
+        // TODO: create player and call PlayerIsReady()
+    }
+
+    [Command]
+    public void CmdDisplayClientMessage()
+    {
+        var NewChatLine = GameObject.Instantiate(ChatLineModel);
+        NewChatLine.transform.SetParent(ChatContainer);
+
+    }
+}
